a143b47cb9c90273b3d8aa489334f5c5b348fe61
Tomas Trujillo <tomastru114@gmail.com>
2024-05-16 16:14:11 +0200

    Fix pagination error

--- src/app/auth/firstConfigurations/page.tsx
+++ src/app/auth/firstConfigurations/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import FirstPage from "@/components/pagination/FirstPage";
+import { FourthPage } from "@/components/pagination/FourthPage";
 import SecondPage from "@/components/pagination/SecondPage";
 import ThirdPage from "@/components/pagination/ThirdPage";
 import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Pagination, Tooltip } from "@nextui-org/react";
@@ -11,9 +12,9 @@ export default function FirstConfiguration() {
 
     const [currentPage, setCurrentPage] = useState(1);
     const pagesTitles = [
-        {title: "Add your accounts", tooltip: "Add the accounts where you manage your money.", tooltip2:"Example : Bank, PayPal, Cash..."},
-        {title: "Add your income types", tooltip: "Add your sources of income.", tooltip2:"Example: Work, Freelance, Gifts..."},
-        {title: "Add your expenses types", tooltip: "Add your main expenses.", tooltip2:"Example: Groceries, Entertainment, Healthcare..."},
+        { title: "Add your accounts", tooltip: "Add the accounts where you manage your money.", tooltip2: "Example : Bank, PayPal, Cash..." },
+        { title: "Add your income types", tooltip: "Add your sources of income.", tooltip2: "Example: Work, Freelance, Gifts..." },
+        { title: "Add your expenses types", tooltip: "Add your main expenses.", tooltip2: "Example: Groceries, Entertainment, Healthcare..." },
     ]
 
     return (
@@ -23,27 +24,34 @@ export default function FirstConfiguration() {
                     <div className="flex flex-col items-center gap-5">
                         <Pagination
                             initialPage={1}
-                            total={3}
+                            total={4}
                             page={currentPage}
                             onChange={setCurrentPage}
                         />
                         <div className="flex gap-2">
                         </div>
                         <div className="flex flex-col items-center text-center">
-                            <h4 className="font-bold text-xl">Lets start your first configuration</h4>
-                            <article className="flex items-center">
-                                <span>{pagesTitles[currentPage-1].title}</span>
-                                <Tooltip content={
-                                    <div className="flex flex-col">
-                                        <span className="mb-2">{pagesTitles[currentPage-1].tooltip}</span>
-                                        <span>{pagesTitles[currentPage-1].tooltip2}</span>
-                                    </div>
-                                }>
-                                    <div className="inline-block relative rounded-large bg-default-200 p-1 ms-2">
-                                        <svg className="w-3" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52" enableBackground="new 0 0 52 52"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M26.7,42.8c0.8,0,1.5,0.7,1.5,1.5v3.2c0,0.8-0.7,1.5-1.5,1.5h-3.2c-0.8,0-1.5-0.7-1.5-1.5v-3.2 c0-0.8,0.7-1.5,1.5-1.5H26.7z"></path> <path d="M28.2,35.1c0-2.1,1.3-4,3.1-4.8h0.1c5.2-2.1,8.8-7.2,8.8-13.2c0-7.8-6.4-14.2-14.2-14.2 c-7.2,0-13.2,5.3-14.2,12.2v0.1c-0.1,0.9,0.6,1.6,1.5,1.6h3.2c0.8,0,1.4-0.5,1.5-1.1v-0.2c0.7-3.7,4-6.5,7.9-6.5 c4.5,0,8.1,3.6,8.1,8.1c0,2.1-0.8,4-2.1,5.5l-0.1,0.1c-0.9,1-2.1,1.6-3.3,2c-4,1.4-6.7,5.2-6.7,9.4v1.5c0,0.8,0.6,1.4,1.4,1.4h3.2 c0.8,0,1.6-0.6,1.6-1.5L28.2,35.1z"></path> </g> </g></svg>
-                                    </div>
-                                </Tooltip>
-                            </article>
+                            {currentPage == 1 &&
+                                <h4 className="font-bold text-xl">Lets start your first configuration</h4>
+                            }
+                            {currentPage == 4 &&
+                                <h4 className="font-bold text-xl">Your configuration is complete!</h4>
+                            }
+                            {currentPage <= 3 &&
+                                <article className="flex items-center">
+                                    <span>{pagesTitles[currentPage - 1].title}</span>
+                                    <Tooltip content={
+                                        <div className="flex flex-col">
+                                            <span className="mb-2">{pagesTitles[currentPage - 1].tooltip}</span>
+                                            <span>{pagesTitles[currentPage - 1].tooltip2}</span>
+                                        </div>
+                                    }>
+                                        <div className="inline-block relative rounded-large bg-default-200 p-1 ms-2">
+                                            <svg className="w-3" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52" enableBackground="new 0 0 52 52"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M26.7,42.8c0.8,0,1.5,0.7,1.5,1.5v3.2c0,0.8-0.7,1.5-1.5,1.5h-3.2c-0.8,0-1.5-0.7-1.5-1.5v-3.2 c0-0.8,0.7-1.5,1.5-1.5H26.7z"></path> <path d="M28.2,35.1c0-2.1,1.3-4,3.1-4.8h0.1c5.2-2.1,8.8-7.2,8.8-13.2c0-7.8-6.4-14.2-14.2-14.2 c-7.2,0-13.2,5.3-14.2,12.2v0.1c-0.1,0.9,0.6,1.6,1.5,1.6h3.2c0.8,0,1.4-0.5,1.5-1.1v-0.2c0.7-3.7,4-6.5,7.9-6.5 c4.5,0,8.1,3.6,8.1,8.1c0,2.1-0.8,4-2.1,5.5l-0.1,0.1c-0.9,1-2.1,1.6-3.3,2c-4,1.4-6.7,5.2-6.7,9.4v1.5c0,0.8,0.6,1.4,1.4,1.4h3.2 c0.8,0,1.6-0.6,1.6-1.5L28.2,35.1z"></path> </g> </g></svg>
+                                        </div>
+                                    </Tooltip>
+                                </article>
+                            }
                         </div>
                         <Divider />
                     </div>
@@ -58,6 +66,9 @@ export default function FirstConfiguration() {
                     {currentPage === 3 &&
                         <ThirdPage />
                     }
+                    {currentPage === 4 &&
+                        <FourthPage />
+                    }
                 </CardBody>
                 <CardFooter className="justify-between">
                     <Button
@@ -68,16 +79,18 @@ export default function FirstConfiguration() {
                     >
                         Previous
                     </Button>
-                    <Button
-                        size="md"
-                        variant="flat"
-                        color="primary"
-                        onPress={() => setCurrentPage((prev) => (prev < 10 ? prev + 1 : prev))}
-                    >
-                        Next
-                    </Button>
+                    {currentPage <= 3 &&
+                        <Button
+                            size="md"
+                            variant="flat"
+                            color="primary"
+                            onPress={() => setCurrentPage((prev) => (prev < 10 ? prev + 1 : prev))}
+                        >
+                            Next
+                        </Button>
+                    }
                 </CardFooter>
             </Card>
-        </div>
+        </div >
     )
 }
